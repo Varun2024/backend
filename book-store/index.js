9ed96b8926309af1756e417642d674d274d81fc1
Varun2024 <marvellous079@gmail.com>
2026-04-05 01:38:22 +0530

    middlewares

--- book-store/index.js
+++ book-store/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { json } = require("node:stream/consumers");
-
+const fs = require("node:fs")
 const app = express();
 const PORT = 8000;
+
+// middleware
+app.use(express.json());
+
+app.use(function(req,res,next){
+  const log = `\n[${Date.now()}  ${req.method} ${req.path}]`
+  fs.appendFileSync('logs.txt',log,'utf-8')
+  next()
+})
+
+
 
 // in memory db
 const books = [
@@ -24,15 +34,45 @@ app.get("/books", (req, res) => {
   res.json(books);
 });
 
+// fetch
 app.get("/books/:id", (req, res) => {
   const id = parseInt(req.params.id);
-
-  if (isNaN(id))
-    return res.status(400).json({error: `Number only`})
   const book = books.find((e) => e.id === id); //SELECT * from books where id ={id}
 
+  //   bad request
+  if (isNaN(id)) return res.status(400).json({ error: `Number only` });
+
   if (!book) return res.status(404).json(`Book wth ${id} does not exists!`);
-  return res.json(book)
+  return res.json(book);
+});
+
+// for adding books
+app.post("/books", (req, res) => {
+  const { title, author } = req.body;
+  if (!title || title === "")
+    return res.status(400).json({error:`title required`});
+  if (!author || author === "")
+    return res.status(400).json({error:`author required`});
+  const id = books.length + 1;
+  const book = {id,title,author}
+  books.push(book)
+  return res.status(201).json({ messgae: "Book added" ,id});
+});
+
+// deleting
+app.delete("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = books.find((e) => e.id === id); //SELECT * from books where id ={id}
+  const indexToDelete = books.findIndex((e) => e.id === id);
+  //   bad request
+  if (isNaN(id)) return res.status(400).json({ error: `Number only` });
+
+  if (indexToDelete < 0)
+    return res.status(404).json(`Book with id ${id} does not exists!`);
+
+  books.splice(indexToDelete, 1);
+
+  return res.status(200).json({ message: "Book deleted" });
 });
 
 app.listen(PORT, () => console.log(`Http server running at port ${PORT}`));
